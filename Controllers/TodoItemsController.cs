a6c8163d456296c337cc0656fce6fb6e530497aa
Bkaes <bkaes8@gmail.com>
2020-02-26 17:47:25 -0500

    Forgot to commit during lab monday

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -2,21 +2,30 @@ using Todo.Api.Models;
 
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Todo.Api.Services;
+using System;
 
 namespace Todo.Api.Controllers 
 {
     [ApiController]
-    [Route("api.todos")]
+    [Route("api/todos")]
     public class TodoItemsController : ControllerBase
     {
+    private ITodoRepository respository;
+    public TodoItemsController(ITodoRepository todoRepository)
+    {
+     respository = todoRepository ??
+        throw new ArgumentNullException(nameof(todoRepository));   
+    }
      [HttpGet]
      public IActionResult GetTodos()
      {
-         var result = new List<TodoItem>(){
-             new TodoItem{ Id = 1, Action="Thing one.", IsDone=true},
-             new TodoItem{ Id = 2, Action="Thing two.", IsDone=false},
-         };
+         var result = respository.GetAllTodoItems();
+             if (result == null)
+             {
+                 return NotFound();
+             }  
          return Ok(result);
-     }   
-    }
+        }   
+}
 }
